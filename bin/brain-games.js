c49ac71e4e0f7eb610b3794d3bc0e77d60b7c797
krllkrbv <krllkrbv@mail.ru>
2025-02-07 13:38:15 +0300

    bin

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-    console.log("Welcome to the Brain Games!");
-    import userName from "../src/cli.js";
+import userName from "../src/games/cli.js";
 
-    userName();
+console.log('Welcome to the Brain Games!');
+
+userName();
