765852738cf0d261981030584a0a5eef2fc4b0dd
krllkrbv <krllkrbv@mail.ru>
2025-02-10 14:36:14 +0300

    src

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,12 +4,14 @@ import getRandomNum from '../utils.js';
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
 const gcd = (a, b) => {
-  while (b !== 0) {
-    let temp = b;
-    b = a % b;
-    a = temp;
+  let num1 = a;
+  let num2 = b;
+  while (num2 !== 0) {
+    const remainder = num2;
+    num2 = num1 % num2;
+    num1 = remainder;
   }
-  return a;
+  return num1;
 };
 
 const getGameRound = () => {
