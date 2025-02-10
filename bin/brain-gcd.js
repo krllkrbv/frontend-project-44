63fe10033bf8250a95a88184dc3a1b8861cd1ca6
krllkrbv <krllkrbv@mail.ru>
2025-02-10 13:46:37 +0300

    bin

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import runGame from '../src/index.js';
-import { getGameRound, gameDescription } from '../src/games/gsd.js';
+import { getGameRound, gameDescription } from '../src/games/gcd.js';
 
 runGame(gameDescription, getGameRound);
